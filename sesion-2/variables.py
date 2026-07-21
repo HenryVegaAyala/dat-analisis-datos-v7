faa0db1f3859a629f4d8eb92d844d9a1bc4c7cfd
Henry Vega <hp.vega@hotmail.com>
2026-07-20 20:10:28 -0500

    add ciclos.py to demonstrate list iteration in Python

---
+++ sesion-2/variables.py
@@ -0,0 +1,43 @@
+# 1. Texto simple -> string
+negocio = "Café python!"
+
+# 2. Texto con comilla simple -> string
+slogan = 'El mejor café de la ciudad'
+
+# 3. número entero -> int
+sillas_disponibles = 30
+
+# 4. número decimal -> float
+precio_cafe = 3.55
+
+# 5. Valor booleano -> bool (True)
+esta_abierto = True
+
+# 6. Valor booleano -> bool (False)
+tiene_wifi = False
+
+# 7. Textos largos
+direccion = "Avenida Angamos 635, Miraflores, Lima, Perú"
+
+# 8. número como texto
+codigo_postal = "500"
+
+# 9. Variables vacías
+proxima_oferta = None
+
+# 10. Variables con caracteres especiales
+emoji_cafe = "☕"
+
+# 11. Línea divisoria
+print("-" * 60)
+
+# 12. Concatenar variables
+concatenar_v1 = negocio + " " + slogan
+print(concatenar_v1)
+
+concatenar_v2 = f"{negocio} {slogan}"
+print(concatenar_v2)
+
+# 13. Concatenación final
+print(f"Ejemplo 1:12: Bienvenido a {concatenar_v2} {emoji_cafe}")
+
