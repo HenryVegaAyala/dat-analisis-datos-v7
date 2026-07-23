e5d8160821a231e16e63cd7d8d7fc8646352cdf9
Henry Vega <hp.vega@hotmail.com>
2026-07-22 20:39:59 -0500

    add ciclos.py to demonstrate list iteration in Python

--- sesion-3/pandas/lectura_archivo/main.py
+++ sesion-3/pandas/lectura_archivo/main.py
@@ -3,5 +3,12 @@ import pandas as pd
 data = pd.read_csv("data_ventas_enero.csv")
 print(data)
 
+print("*" * 60)
+
 data_xlsx = pd.read_excel("data_ventas_enero_xlsx.xlsx")
 print(data_xlsx)
+
+print("*" * 60)
+
+data_hoja1 = pd.read_excel("data_ventas_enero_xlsx.xlsx", sheet_name="Hoja1")
+print(data_hoja1)
