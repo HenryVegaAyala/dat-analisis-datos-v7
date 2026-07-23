9c116dd2a5ab1cbc47db5603d6c29b2b15703211
Henry Vega <hp.vega@hotmail.com>
2026-07-22 21:40:19 -0500

    add ciclos.py to demonstrate list iteration in Python

--- sesion-3/pandas/ordenamiento/main.py
+++ sesion-3/pandas/ordenamiento/main.py
@@ -14,3 +14,7 @@ print("*" * 100)
 
 resultado = df.sort_values(["cantidad", "vendedor"], ascending=[False, True])
 print(resultado)
+
+# Exportar resultado a los archivos necesarios
+resultado.to_csv("resultado.csv")
+resultado.to_excel("resultado.xlsx")
