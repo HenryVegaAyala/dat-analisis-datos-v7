879f5486ba8491c4892ef057c627a71dde3a7810
Henry Vega <hp.vega@hotmail.com>
2026-07-27 22:01:26 -0500

    add ciclos.py to demonstrate list iteration in Python

---
+++ sesion-4/pandas/limpieza_datos/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = pd.read_csv("facturacion.csv")
+
+data["id_cliente"] = data["id_cliente"].fillna("Desconocido")
+print(data)
+
+data_sin_nan = data.dropna()
+print(data_sin_nan)
+
+data_corregida_duplicados = data.drop_duplicates()
+print(data_corregida_duplicados)
