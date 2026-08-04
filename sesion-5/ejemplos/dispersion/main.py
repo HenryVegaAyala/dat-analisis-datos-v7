b0b1757f297f0b483cba786cd736bc842507afba
hvega-visma <henry.vega@visma.com>
2026-08-03 21:34:13 -0500

    grafico

---
+++ sesion-5/ejemplos/dispersion/main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+horas_estudio = [1, 2, 3, 4, 5, 6, 7, 8]
+calificaciones = [13, 15, 15, 16, 17, 18, 19, 20]
+
+plt.scatter(
+    horas_estudio,
+    calificaciones
+)
+
+plt.show()
